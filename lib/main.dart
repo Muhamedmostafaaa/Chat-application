4c447fb796001a65eee830ac64f0ac5befd1c7d4
dell <muhamed_mostafaa12@hotmail.com>
2022-06-12 10:33:15 +0200

    init app

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(myapp());
+}
+class myapp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+
+}
